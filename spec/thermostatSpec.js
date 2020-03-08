3ceb39a242bc67cdfb7df51875473848e576fce8
CodeKrakken <newholm1@hotmail.com>
2020-03-08 15:05:41 +0000

    Revert to single increment temperature changes

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,17 +12,12 @@ describe('Thermostat', function() {
   });
 
   it('increases the temperature', function() {
-    thermostat.up(1);
+    thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
   it('decreases the temperature', function() {
-    thermostat.down(1);
-    expect(thermostat.getCurrentTemperature()).toEqual(19);
-  });
-
-  it('adjusts for input of minus numbers for decrease', function() {
-    thermostat.down(-1);
+    thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 });
